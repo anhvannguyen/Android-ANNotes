83f6a0265f4c120a15c82ca03a635b8150716d67
Anh Van Nguyen <nguyen.anhvan@gmail.com>
2015-12-23 15:35:16 -0800

    create the editor activity/fragment and layout

--- app/src/main/java/me/anhvannguyen/android/annotes/EditorActivity.java
+++ app/src/main/java/me/anhvannguyen/android/annotes/EditorActivity.java
@@ -1,13 +1,21 @@
 package me.anhvannguyen.android.annotes;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class EditorActivity extends Activity {
+public class EditorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+
+        if (savedInstanceState == null) {
+            EditorFragment fragment = new EditorFragment();
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.note_editor_container, fragment)
+                    .commit();
+        }
     }
 }
--- app/src/main/java/me/anhvannguyen/android/annotes/MainActivity.java
+++ app/src/main/java/me/anhvannguyen/android/annotes/MainActivity.java
@@ -1,5 +1,6 @@
 package me.anhvannguyen.android.annotes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -22,8 +23,10 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                Intent intent = new Intent(getApplicationContext(), EditorActivity.class);
+                startActivity(intent);
             }
         });
     }
