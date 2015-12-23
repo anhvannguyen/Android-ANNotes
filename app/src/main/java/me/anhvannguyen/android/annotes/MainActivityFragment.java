fef0fe443d5bda217c5fcc4ca579b65b9917fed1
Anh Van Nguyen <nguyen.anhvan@gmail.com>
2015-12-22 19:03:44 -0800

    set adapater for RecyclerView

--- app/src/main/java/me/anhvannguyen/android/annotes/MainActivityFragment.java
+++ app/src/main/java/me/anhvannguyen/android/annotes/MainActivityFragment.java
@@ -1,7 +1,9 @@
 package me.anhvannguyen.android.annotes;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,12 +13,24 @@ import android.view.ViewGroup;
  */
 public class MainActivityFragment extends Fragment {
 
+    private RecyclerView mNotesRecyclerView;
+    private NotesRecyclerAdapter mNotesRecyclerAdapter;
+
+
+
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView =  inflater.inflate(R.layout.fragment_main, container, false);
+
+        mNotesRecyclerAdapter = new NotesRecyclerAdapter(getActivity());
+
+        mNotesRecyclerView = (RecyclerView) rootView.findViewById(R.id.notes_recyclerview);
+        mNotesRecyclerView.setAdapter(mNotesRecyclerAdapter);
+
+        return rootView;
     }
 }
--- app/src/main/java/me/anhvannguyen/android/annotes/NotesRecyclerAdapter.java
+++ app/src/main/java/me/anhvannguyen/android/annotes/NotesRecyclerAdapter.java
@@ -1,16 +1,22 @@
 package me.anhvannguyen.android.annotes;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 
 
 public class NotesRecyclerAdapter extends RecyclerView.Adapter<NotesRecyclerAdapter.ViewHolder> {
+    private Context mContext;
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         public ViewHolder(View itemView) {
             super(itemView);
         }
+    }
+
+    public NotesRecyclerAdapter(Context context) {
+        mContext = context;
     }
 
     @Override
