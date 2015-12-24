c3ee962aef133914c9dcdd2e6408fbc3a72cf670
Anh Van Nguyen <nguyen.anhvan@gmail.com>
2015-12-23 22:46:21 -0800

    create db table for helper

--- app/src/main/java/me/anhvannguyen/android/annotes/data/DBOpenHelper.java
+++ app/src/main/java/me/anhvannguyen/android/annotes/data/DBOpenHelper.java
@@ -9,17 +9,25 @@ public class DBOpenHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "annotes.db";
     private static final int DATABASE_VERSION = 1;
 
+    private static final String SQL_CREATE_NOTES_TABLE =
+            "CREATE TABLE " + NotesContract.NoteEntry.TABLE_NAME + " (" +
+                    NotesContract.NoteEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    NotesContract.NoteEntry.COLUMN_TEXT + " TEXT, " +
+                    NotesContract.NoteEntry.COLUMN_DATE_CREATED + " TEXT DEFAULT CURRENT_TIMESTAMP" +
+                    ")";
+
     public DBOpenHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(SQL_CREATE_NOTES_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS " + NotesContract.NoteEntry.TABLE_NAME);
+        onCreate(db);
     }
 }
