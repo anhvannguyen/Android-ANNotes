24da49954c8f53795f49edbef54ac9d431b30148
Anh Van Nguyen <nguyen.anhvan@gmail.com>
2015-12-23 19:03:12 -0800

    create a helper class for database

---
+++ app/src/main/java/me/anhvannguyen/android/annotes/data/DBOpenHelper.java
@@ -0,0 +1,25 @@
+package me.anhvannguyen.android.annotes.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBOpenHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "annotes.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DBOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
