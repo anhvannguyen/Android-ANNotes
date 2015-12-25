42ff0a71b435300311387f11cbe70954d8cc36e9
Anh Van Nguyen <nguyen.anhvan@gmail.com>
2015-12-24 19:17:59 -0800

    create constants for content provider

--- app/src/main/java/me/anhvannguyen/android/annotes/data/NotesContract.java
+++ app/src/main/java/me/anhvannguyen/android/annotes/data/NotesContract.java
@@ -1,14 +1,26 @@
 package me.anhvannguyen.android.annotes.data;
 
+import android.content.ContentResolver;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public class NotesContract {
 
     public static final String CONTENT_AUTHORITY = "me.anhvannguyen.android.annotes";
 
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
     public static final String PATH_NOTE = "note";
 
     public static class NoteEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_NOTE).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_NOTE;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_NOTE;
 
         public static final String TABLE_NAME = "notes";
 
