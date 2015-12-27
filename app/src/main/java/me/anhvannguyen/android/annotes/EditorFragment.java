423823ad39995e0b7e8c60e016b8baf6010fa019
Anh Van Nguyen <nguyen.anhvan@gmail.com>
2015-12-27 00:43:14 -0800

    enable menu options for editor

--- app/src/main/java/me/anhvannguyen/android/annotes/EditorFragment.java
+++ app/src/main/java/me/anhvannguyen/android/annotes/EditorFragment.java
@@ -2,10 +2,16 @@ package me.anhvannguyen.android.annotes;
 
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+
+import me.anhvannguyen.android.annotes.data.NotesContract;
 
 
 /**
@@ -16,6 +22,7 @@ public class EditorFragment extends Fragment {
 
     public EditorFragment() {
         // Required empty public constructor
+        setHasOptionsMenu(true);
     }
 
 
@@ -23,7 +30,32 @@ public class EditorFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_editor, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_editor, container, false);
+
+        return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_editor_fragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_save_note:
+                Snackbar.make(getView(), "Save Pressed", Snackbar.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_delete_note:
+                Snackbar.make(getView(), "Delete Pressed", Snackbar.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+
     }
 
 }
