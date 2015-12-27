ca01b121fd0f2a464b396018951d212bcfa13f3d
Anh Van Nguyen <nguyen.anhvan@gmail.com>
2015-12-27 00:13:28 -0800

    add the menu option callbacks

--- app/src/main/java/me/anhvannguyen/android/annotes/EditorActivity.java
+++ app/src/main/java/me/anhvannguyen/android/annotes/EditorActivity.java
@@ -2,6 +2,8 @@ package me.anhvannguyen.android.annotes;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class EditorActivity extends AppCompatActivity {
 
@@ -17,5 +19,27 @@ public class EditorActivity extends AppCompatActivity {
                     .add(R.id.note_editor_container, fragment)
                     .commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
