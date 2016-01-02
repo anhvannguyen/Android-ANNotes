4896218c8719c25ac329be592c6a3b5780639fb7
Anh Van Nguyen <nguyen.anhvan@gmail.com>
2016-01-01 19:52:11 -0800

    load note data into editor when user select item from list

--- app/src/main/java/me/anhvannguyen/android/annotes/EditorFragment.java
+++ app/src/main/java/me/anhvannguyen/android/annotes/EditorFragment.java
@@ -2,10 +2,15 @@ package me.anhvannguyen.android.annotes;
 
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -20,8 +25,18 @@ import me.anhvannguyen.android.annotes.data.NotesContract;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class EditorFragment extends Fragment {
+public class EditorFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
     public static final String NOTE_URI = "note_uri";
+    public static final int NOTE_LOADER = 0;
+
+    private static final String[] NOTE_PROJECTION = {
+            NotesContract.NoteEntry._ID,
+            NotesContract.NoteEntry.COLUMN_TEXT,
+            NotesContract.NoteEntry.COLUMN_DATE_CREATED
+    };
+    public static final int COL_ID = 0;
+    public static final int COL_TEXT = 1;
+    public static final int COL_DATE_CREATED = 2;
 
     private EditText mNoteEditText;
     private Uri mNoteUri;
@@ -49,6 +64,12 @@ public class EditorFragment extends Fragment {
         }
 
         return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        getLoaderManager().initLoader(NOTE_LOADER, null, this);
+        super.onActivityCreated(savedInstanceState);
     }
 
     @Override
@@ -82,4 +103,30 @@ public class EditorFragment extends Fragment {
 
     }
 
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        if (mNoteUri != null) {
+            return new CursorLoader(
+                    getActivity(),
+                    mNoteUri,
+                    NOTE_PROJECTION,
+                    null,
+                    null,
+                    null
+            );
+        }
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        if (data != null && data.moveToFirst()) {
+            mNoteEditText.setText(data.getString(COL_TEXT));
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
 }
