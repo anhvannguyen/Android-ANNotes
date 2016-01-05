33c29f66aacdcd20ae3789f884f976b15f02f3cf
Anh Van Nguyen <nguyen.anhvan@gmail.com>
2016-01-04 16:26:23 -0800

    create setting activity class

---
+++ app/src/main/java/me/anhvannguyen/android/annotes/SettingsActivity.java
@@ -0,0 +1,25 @@
+package me.anhvannguyen.android.annotes;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public class SettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        getFragmentManager().beginTransaction()
+                .replace(R.id.settings_container, new SettingsFragment())
+                .commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_general);
+        }
+    }
+}
